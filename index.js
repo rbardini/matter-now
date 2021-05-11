151a19260116803e4fab73ec9140b71ae6ceefa4
Rafael Bardini <rbardini@users.noreply.github.com>
2021-05-11 13:52:14 +0200

    feat(attr): support custom front matter attributes via `-a, --attr` option (#10)

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const grayMatter = require('gray-matter');
 const moment = require('moment');
 
 const hasMatter = (matter) => matter.isEmpty || Object.keys(matter.data).length > 0;
-const hasDate = (matter) => 'date' in matter.data;
+const hasAttr = (matter, attr) => attr in matter.data;
 
-module.exports = (files, { format }) => files.forEach((file) => {
+module.exports = (files, { attr, format }) => files.forEach((file) => {
   const matter = grayMatter.read(file, {});
 
-  if (!hasMatter(matter) || hasDate(matter)) {
+  if (!hasMatter(matter) || hasAttr(matter, attr)) {
     return;
   }
 
   const date = moment().format(format);
-  const newMatter = `${matter.matter}\ndate: ${date}`;
+  const newMatter = `${matter.matter}\n${attr}: ${date}`;
   const newData = `---${newMatter}\n---\n${matter.content}`;
 
   fs.writeFileSync(file, newData);
